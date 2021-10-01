b65cc13f8e4966b642d77cdaa549842678db8ed8
Lone Wolf <binitakarki3355@gmail.com>
2021-10-01 09:23:47 +0545

    DAta types

---
+++ index.js
@@ -0,0 +1,31 @@
+//let a = 1;
+//let b = "Hello World";
+//let c = true;
+//let d = 1.5;
+//let e = new Date();
+//let f = null;
+
+//console.log(typeof a);
+//console.log(typeof b);
+//console.log(typeof c);
+//console.log(typeof d);
+//console.log(typeof e);
+//console.log(typeof f);
+
+//const example = "Hello World";
+//console.log(example);
+
+ let person = {
+     Name: "Yajindra",
+     Age: 21,
+     Address: "Kathmandu",
+     location: {
+         lat: 27.3546565,
+         lon: 75.4565743
+     }
+ };
+ console.log("This person name is: ", person.Name);
+ console.log("The Location is: ", person.location);
+ console.log("The Location is: ", person.location.lat);
+ console.log("The Location is: ", person.location.lon);
+
